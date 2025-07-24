db29a2e32a02125120ea82a7e9a740f4675a4348
Deekshitha26-ai <dpallala@lakeheadu.ca>
2025-07-24 18:40:53 -0400

    Accounts Table Update endpoint

--- src/main/scala/com/axiom/patienttracker/repositories/DoctorRepository.scala
+++ src/main/scala/com/axiom/patienttracker/repositories/DoctorRepository.scala
@@ -8,6 +8,8 @@ import com.axiom.patienttracker.domain.data.Doctor
 
 trait DoctorRepository:
     def create(doctor: Doctor): Task[Doctor]
+    def update(providerId: String, op: Doctor => Doctor): Task[Doctor]
+    def getByProviderId(providerId: String): Task[Option[Doctor]]
 
 class DoctorRepositoryLive(quill: Quill.Postgres[SnakeCase]) extends DoctorRepository:
     import quill.* //gives us access to methods such as run, query, filter or lift
@@ -25,6 +27,26 @@ class DoctorRepositoryLive(quill: Quill.Postgres[SnakeCase]) extends DoctorRepos
         The RETURNING in quill gives us the columns in the exact order of our case class.
         Our scala compiler doesnot know the which string values belongs to which column but our quill knows
          */
+        //Update method 
+    // def getByProviderId(providerId: String): Task[Option[Doctor]]
+    override def getByProviderId(providerId: String): Task[Option[Doctor]] =
+            run {
+                query[Doctor].filter(_.providerId == lift(providerId))
+            }.map(_.headOption)
+
+    
+    override def update(providerId: String, op: Doctor => Doctor): Task[Doctor] =
+            for {
+                current <- getByProviderId(providerId)
+                .someOrFail(new RuntimeException(s"Could not update: missing Provider ID: $providerId"))
+                updated <- run {
+                query[Doctor]
+                    .filter(_.providerId == lift(providerId))
+                    .updateValue(lift(op(current)))
+                    .returning(d => d)
+                }
+            } yield updated
+
 
 object DoctorRepositoryLive:
     val layer = ZLayer {
--- src/main/scala/com/axiom/patienttracker/services/DoctorService.scala
+++ src/main/scala/com/axiom/patienttracker/services/DoctorService.scala
@@ -4,15 +4,29 @@ import zio.*
 import com.axiom.patienttracker.domain.data.Doctor
 import com.axiom.patienttracker.http.requests.CreateDoctorRequest
 import com.axiom.patienttracker.repositories.DoctorRepository
+import com.axiom.patienttracker.http.requests.UpdateDoctorRequest
 
 //Logic
 // in between the HTTP layer and the Database layer
 trait DoctorService:
     def create(req: CreateDoctorRequest): Task[Doctor]
+    def update(req: UpdateDoctorRequest): Task[Doctor]
 
 class DoctorServiceLive private (repo: DoctorRepository) extends DoctorService:
     override def create(req: CreateDoctorRequest): Task[Doctor] = 
         repo.create(req.toDoctor(-1L))
+
+    private def applyUpdates(existing: Doctor, update: UpdateDoctorRequest): Doctor =
+        existing.copy(
+        name = update.name.getOrElse(existing.name)
+        
+        )
+    override def update(req: UpdateDoctorRequest): Task[Doctor] = 
+        req.providerId match
+        case Some(id) =>
+            repo.update(id, existing => applyUpdates(existing, req))
+        case None =>
+            ZIO.fail(new RuntimeException("Missing providerId in update request"))
 
 
 object DoctorServiceLive:
